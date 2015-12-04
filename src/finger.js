81050500ef8394817277ac91dcdd57c6de09dd85
Juha Karttunen <juha.karttunen@siili.fi>
2015-12-04 12:54:16 +0200

    First commit

---
+++ src/finger.js
@@ -0,0 +1,16 @@
+var body = document.body;
+//body.style.cursor = 'none';
+var img = document.createElement('img');
+img.src= chrome.extension.getURL('/images/finger.png');
+img.id = 'sormi';
+img.style.position = 'fixed';
+img.style.top = '0px';
+img.style.left = '0px';
+body.appendChild(img);
+
+body.addEventListener('mousemove', function(o) {
+    var sormi = document.getElementById('sormi');
+    sormi.style.left= o.screenX + 1 + 'px';
+    sormi.style.top= o.screenY - 400 + 'px';
+    console.log(o.screenX, o.screenY);
+});
