a9ff2bd0fb3c3b6a3daefaffedb5f4666b5c47e7
Juha Karttunen <juha.karttunen@siili.fi>
2015-12-10 12:58:14 +0200

    Fix #7

--- src/finger.js
+++ src/finger.js
@@ -4,6 +4,7 @@ var img = document.createElement('img');
 img.src= chrome.extension.getURL('/images/finger.png');
 img.id = 'sormi';
 img.style.position = 'fixed';
+img.style.zIndex = '999999';
 img.style.top = '0px';
 img.style.left = '0px';
 body.appendChild(img);
@@ -11,6 +12,5 @@ body.appendChild(img);
 body.addEventListener('mousemove', function(o) {
     var sormi = document.getElementById('sormi');
     sormi.style.left= o.screenX + 1 + 'px';
-    sormi.style.top= o.screenY - 400 + 'px';
-    console.log(o.screenX, o.screenY);
+    sormi.style.top= o.screenY - 337 + 'px';
 });
